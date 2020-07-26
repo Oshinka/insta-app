5c51f4d8ee9d203d3902eb4f035fc41fdfdebe19
Oshinka <oshinka94@gmail.com>
2020-07-26 20:10:35 +0530

    Updates

---
+++ precache-manifest.5b4560dfc92cad5d408ad1c524579c47.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874279c337adb9ad871586e4953d5248",
+    "url": "/insta-app/index.html"
+  },
+  {
+    "revision": "5a33afd432f15f5ef420",
+    "url": "/insta-app/static/css/main.c9b0b82e.chunk.css"
+  },
+  {
+    "revision": "405d0dbb512b6f86a256",
+    "url": "/insta-app/static/js/2.e40a9c2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/insta-app/static/js/2.e40a9c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a33afd432f15f5ef420",
+    "url": "/insta-app/static/js/main.93e4317f.chunk.js"
+  },
+  {
+    "revision": "515cacd5f42323205174",
+    "url": "/insta-app/static/js/runtime-main.9d0e7ece.js"
+  },
+  {
+    "revision": "902c4365b6e050acc85eab3d8c238d94",
+    "url": "/insta-app/static/media/explore.902c4365.svg"
+  },
+  {
+    "revision": "0ed5fbdb544618b2919b98c91fe23108",
+    "url": "/insta-app/static/media/home.0ed5fbdb.svg"
+  },
+  {
+    "revision": "3388b1a17a38b9833ed762278f327330",
+    "url": "/insta-app/static/media/instagram.3388b1a1.svg"
+  },
+  {
+    "revision": "16069ca879d0d56c683b6f76710d7f6a",
+    "url": "/insta-app/static/media/user.16069ca8.svg"
+  }
+]);
